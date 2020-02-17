e5ec6d9070a2c80469877b3784ea5b8ea57c4a59
Chris H <58816114+akainoto@users.noreply.github.com>
2020-02-18 04:04:17 +0800

    Initial Commit

---
+++ Closure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Closure
+//
+//  Created by Chris Hennessy on 1/4/20.
+//  Copyright © 2020 Chris Hennessy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
